5732704a2a1ae6ed1656ea05e56c1fbeb0e595b2
TimF <timothy.faber1808@gmail.com>
2025-06-26 14:01:26 +0200

    Added Subtract function to the api

--- src/api.py
+++ src/api.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 def add_numbers(a, b):
     return a + b
 
+def subtract_numbers(a, b):
+    return a - b
+
 @app.route('/')
 def hello():
-    return "Welcome to the Kubernetes Test API"
+    return "Welcome to the Calculator API"
 
 # Neue API-Route zum Addieren von zwei Zahlen
 @app.route('/add', methods=['GET'])
@@ -25,6 +28,20 @@ def add():
             })
     except (TypeError, ValueError):
         return jsonify({'error': 'Bitte gueltige Zahlen angeben, z.B. /add?a=3&b=4'}), 400
+    
+@app.route('/subtract', methods=['GET'])
+def add():
+    try:
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b')) 
+        result = subtract_numbers(a, b)
+        return jsonify({
+            'a': a,
+            'b': b,
+            'result': result
+            })
+    except (TypeError, ValueError):
+        return jsonify({'error': 'Bitte gueltige Zahlen angeben, z.B. /subtract?a=3&b=4'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
